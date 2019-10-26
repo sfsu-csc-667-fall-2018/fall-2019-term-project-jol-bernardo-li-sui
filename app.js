7d0477044422fc116a4d405ba4b4b5be5c6d2f0a
Chris Jol <jol.chris2@gmail.com>
2019-10-26 13:53:10 -0700

    finished initial setup for milestone 2

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 if(process.env.NODE_ENV === 'development') {
     require("dotenv").config();
 }
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const testsRouter = require('./routes/tests');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tests', testsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

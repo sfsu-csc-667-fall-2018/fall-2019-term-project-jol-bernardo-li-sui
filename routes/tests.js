2fc20fd27803e9a8faabad032bcb7e3696b86dda
Chris Jol <jol.chris2@gmail.com>
2019-10-27 16:10:28 -0700

    added pug functionality on /tests endpoint

--- routes/tests.js
+++ routes/tests.js
@@ -5,7 +5,10 @@ const db = require('../db');
 router.get('/', (req, res, next) => {
     db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
         .then( _ => db.any('SELECT * FROM test_table'))
-        .then( results => res.json( results ))
+        .then(
+            // results => res.send(results)
+            results => res.render('tests', {result: results})
+        )
         .catch( error => {
             console.log( error )
             res.json({ error })

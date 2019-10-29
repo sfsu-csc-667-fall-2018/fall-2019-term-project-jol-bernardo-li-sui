ad2139279d7eb227b640863f58016dba242abff8
Chris Jol <jol.chris2@gmail.com>
2019-10-29 13:57:19 -0700

    fix to config.js syntax

--- config/config.js
+++ config/config.js
@@ -6,8 +6,10 @@ module.exports = {
         "use_env_variable": "DATABASE_URL",
         "dialect": "postgres"
     },
-    "test": { "use_env_variable": "DATABASE_URL" },
-    "dialect": "postgres",
+    "test": { 
+        "use_env_variable": "DATABASE_URL",
+        "dialect": "postgres"
+    },
     "production": 
     {
         "use_env_variable": "DATABASE_URL",

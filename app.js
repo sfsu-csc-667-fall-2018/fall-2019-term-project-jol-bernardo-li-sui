125b7e9ebfcee5f98e1b25ac9e8b155329886c3b
Chris Jol <jol.chris2@gmail.com>
2019-11-11 15:21:00 -0800

    organized and optimized layout for index, cleaned up css a bit

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tests', testsRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
+app.use('/', testsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

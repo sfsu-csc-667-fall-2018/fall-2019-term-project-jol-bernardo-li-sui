3e104fc45ee6e316d79d7b16e3713e46e368861e
Chris Jol <jol.chris2@gmail.com>
2019-11-14 18:37:09 -0800

    began implementation of gamesession, also implemented sign in scree

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 // login
 router.get('/login',(req, res) => res.render('login'));
 // Register

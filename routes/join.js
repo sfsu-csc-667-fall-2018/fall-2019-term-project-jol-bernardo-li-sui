9c6722454d22ba359a419c3514fab20fb76475c7
Nicole <nicolebernardo96@gmail.com>
2019-11-19 19:30:22 -0800

    started designing lobby page

--- routes/join.js
+++ routes/join.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const Pool = require('pg').Pool
 const router = express.Router();
 
 /* join page. */
 router.get('/join', function(req, res, next) {
-  res.render('joinsession', { title: 'Express' });
+  res.render('lobby', { title: 'Express' });
 });
 
 module.exports = router;

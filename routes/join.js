dd389af7c52c25d7f94f51b43f0e38b5c567523c
Chris Jol <jol.chris2@gmail.com>
2019-12-04 16:01:41 -0800

    added package.json to gitignore, began implementation of lobby

--- routes/join.js
+++ routes/join.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* join page. */
-router.get('/join', function(req, res, next) {
+router.get('/lobby', function(req, res, next) {
   res.render('lobby', { title: 'Express' });
 });
 

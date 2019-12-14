c98657ec483d29e790416ac6b6c5dc8bb9ebbc70
Gangzhaorige <gangzhaorige.li@gmail.com>
2019-12-13 16:08:50 -0800

    updated of drawing 7 cards for a player

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,7 @@
 const {
     USER_JOINED
 } = require('../src/events')
+const db = require('../db');
 const express = require('express');
 const router = express.Router();
 const models = require("../models")
@@ -8,9 +9,15 @@ const models = require("../models")
 router.get('/game/:id', (req, res, next) => {
     models.Game.findOne({where: {id: req.params.id}}).then( game => {
         models.Player.findOne({where: {userId: req.user.id, gameId: req.params.id}}).then( player => {
-            models.Card.findAll({where: {played: false, playerId: player.dataValues.id}}).then(card => {
-                res.render('gamesession', {gameName: game.dataValues.gameName, cards: card})
-            }) 
+            models.Card.findAll({
+                where:{played: false, playerId: null, deckId: game.dataValues.deckId},
+                limit: 7
+            }).then(cards => {
+                cards.map(card => {
+                    card.update({playerId: player.dataValues.id})
+                })
+                res.render('gamesession', {gameName: game.dataValues.gameName})
+            })
         })
     })
 })
